22d2003f687c69a5b7c1e25dd83737911cd6651f
Katherine Cheng <katcheng@ucdavis.edu>
2022-10-05 19:21:11 -0700

    Create questionnaire

---
+++ questionnaire.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 19:14:35 2022
+
+@author: User
+"""
+
+#https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv
+#all missing values are marked with a 0 for only columns 1-5
+
+#1. save the above as a .csv file
+
+#2. read the CSV into a df
+
+#3. check the size of the df
+
+#4. check the first 5 rows
+
+#5. index out the first column
+
+#6. count the number of missing values for each column
+
+#7. replace '0' values with 'np.nan'
+
+#8. drop rows with missing values
+
+#9. check the size of the final df
