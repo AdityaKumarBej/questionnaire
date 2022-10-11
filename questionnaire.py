291867e7927a5be3f21da2b75f1b4c7193856f4d
Aditya Bej <adityabej@Adityas-MacBook-Pro.local>
2022-10-11 09:55:06 -0700

    done
    done
    !q
    
    clear
    cls
    
    ~
    echo ab

--- questionnaire.py
+++ questionnaire.py
@@ -12,16 +12,37 @@ Created on Wed Oct  5 19:14:35 2022
 
 #2. read the CSV into a df
 
-#3. check the size of the df
+#3. check the size of the df (size)
 
-#4. check the first 5 rows
+#4. check the first 5 rows (iterating via index or direct first 5)
 
-#5. index out the first column
+#5. index out the first column (index_col=0)
 
-#6. count the number of missing values for each column
+#6. count the number of missing values for each column (iterate all columns, find df['column'] == 0 (find sum))
 
 #7. replace '0' values with 'np.nan'
 
 #8. drop rows with missing values
 
 #9. check the size of the final df
+
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('pima-indians-diabetes.csv', header=None) #index first column
+
+#print(df.to_string())
+
+print('initial size', df.size)
+print('first 5 rows', df.head(5))
+print('index first columnn', df.iloc[:,0])
+
+count = (df[4] == 0).sum()
+print('count of 0s', count)
+
+df.replace(0, np.nan, inplace=True)
+print('checking', df)
+
+df = df.dropna()
+print(df)
+print('final size', df.size)
